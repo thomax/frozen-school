532dff53801c123520a9aeaaff2f8a27092e511b
Thomas Drevon <thomas@drevon.no>
2023-11-17 15:08:05 +0100

    Wire up kitchen from hallway

--- src/lib/locations/thomax/DarkHallway.svelte
+++ src/lib/locations/thomax/DarkHallway.svelte
@@ -8,4 +8,5 @@
   <div>It's dark and freezing, and I can't see the end of the hall</div>
   <button on:click={() => goToLocation('fc307')}>Back to the frozen classroom</button>
   <button on:click={() => goToLocation('so')}>Strange office</button>
+  <button on:click={() => goToLocation('ki')}>Kitchen</button>
 </div>
