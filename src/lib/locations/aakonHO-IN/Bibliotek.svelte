665161762ce2189b1c80f4106a9dd2d088f8c40a
Håkon Hove-Indrebø <hahoa010@osloskolen.no>
2023-11-24 15:50:47 +0100

    add book to library

--- src/lib/locations/aakonHO-IN/Bibliotek.svelte
+++ src/lib/locations/aakonHO-IN/Bibliotek.svelte
@@ -3,26 +3,41 @@
   import {goToLocation} from '../../dataStores/locationStore.js'
   export let location
   import bibliotekUrl from '../../../assets/aakonHO-IN/bibliotek.png'
+  import bookUrl from '../../../assets/aakonHO-IN/book.png'
+  const externalSiteUrl = 'https://portal.skoleplattform.no/verktoy-v/';
   let mainElement
+  let book
 
 
   onMount(() => {
     mainElement.style.background = `url('${bibliotekUrl}')  no-repeat center center fixed`
     mainElement.style.backgroundSize = 'cover'
   })
+
+  function handleViewToggle() {
+    book = true;
+  }
+  
 </script>
+
+<div class="libraryBox" bind:this={mainElement}>
 <h1>{location.title}</h1>
 <button on:click={() => goToLocation('dh')}>Exit to hallway</button>
-<div class = "libraryBox" bind:this={mainElement}>
-  
+
+<a href={externalSiteUrl} target="_blank">
+  <img src={bookUrl} alt="book" width=100 height=70 on:click={handleViewToggle} style="position: absolute; top: 50%; left: 50%;" >
+</a>
 </div>
 
 
 <style>
   .libraryBox {
-    height: 1000px;
+    height: 90vh;
     width: auto;
+    }
+  h1{
+color:black;
+background-color: aqua;
+}
 
-
-  }
 </style>
