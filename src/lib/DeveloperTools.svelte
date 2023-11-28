c208581bbd5b5f1690aaea08dd5f06d07c546fc1
Thomas Drevon <thomas@drevon.no>
2023-11-28 16:34:34 +0100

    Improve dev tool output

--- src/lib/DeveloperTools.svelte
+++ src/lib/DeveloperTools.svelte
@@ -36,28 +36,65 @@
 
 <div id="devToolsComponent">
   <h3>Developer Tools</h3>
-  <button on:click={() => handleChangeTemperature(15)}>increase</button>
-  <button on:click={() => handleChangeTemperature(-1)}>decrease</button>
-  <button on:click={() => handleChangeFreezeRate(2)}>Make it colder</button>
-  <button on:click={() => handleChangeFreezeRate(0.5)}>Make it warmer</button>
+  <div>
+    <button on:click={() => handleChangeTemperature(15)}>+ character temp</button>
+    <button on:click={() => handleChangeTemperature(-1)}>- character temp</button>
+  </div>
+  <div>
+    <button on:click={() => handleChangeFreezeRate(2)}>+ freezeRate</button>
+    <button on:click={() => handleChangeFreezeRate(0.5)}>- freezeRate</button>
+  </div>
   <div>
     <h2>Game state</h2>
-    <pre>
-      {JSON.stringify(localGameState, null, 2)}
-    </pre>
+    <table>
+      {#each Object.keys(localGameState) as key}
+        <tr>
+          <th>{key}</th>
+          <td>{localGameState[key]}</td>
+          <td>{typeof localGameState[key]}</td>
+        </tr>
+      {/each}
+    </table>
     <h2>Character</h2>
-    <pre>
-      {JSON.stringify(localCharacter, null, 2)}
-    </pre>
-    <h2>Location</h2>
-    <pre>
-      {JSON.stringify(localLocation, null, 2)}
-    </pre>
+    <table>
+      {#each Object.keys(localCharacter) as key}
+        <tr>
+          <th>{key}</th>
+          <td>{localCharacter[key]}</td>
+          <td>{typeof localCharacter[key]}</td>
+        </tr>
+      {/each}
+    </table>
+    <h2>Location: {localLocation}</h2>
   </div>
 </div>
 
 <style>
   #devToolsComponent {
     border: 2px solid blue;
+  }
+
+  table {
+    width: 60%;
+    table-layout: fixed;
+    margin: auto;
+  }
+  tr {
+    margin: 0;
+    padding: 0;
+  }
+  th {
+    margin: 0;
+    padding: 0px 15px 0px 5px;
+    text-align: left;
+    border-bottom: solid 2px darkolivegreen;
+  }
+  td {
+    margin: 0;
+    padding: 0px 15px 0px 5px;
+    text-align: left;
+    vertical-align: top;
+    font-size: 1rem;
+    border-bottom: solid 2px darkolivegreen;
   }
 </style>
