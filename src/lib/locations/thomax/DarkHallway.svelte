acaeff04a3bc2e81eec6d3420d2a2ba8f4be9d07
Nicolai Hindenes Johannessen <nijoa005@osloskolen.no>
2023-11-29 08:41:20 +0100

    opprettet locker room

--- src/lib/locations/thomax/DarkHallway.svelte
+++ src/lib/locations/thomax/DarkHallway.svelte
@@ -14,4 +14,5 @@
   <button on:click={() => goToLocation('toi')}>Toilets</button>
   <button on:click={() => goToLocation('caf')}>Cafeteria</button>
   <button on:click={() => goToLocation('hall')}>Navigation test</button>
+  <button on:click={() => goToLocation('lr')}>Locker room</button>
 </div>
