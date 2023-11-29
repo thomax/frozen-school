afe74ccb7e3ba1fc780480a8bdd96802384d481d
Teodor Eiken Ørjansen <teoea001@osloskolen.no>
2023-11-29 08:33:30 +0100

    fix changes

--- src/lib/locations/teodororjansen/Toilets.svelte
+++ src/lib/locations/teodororjansen/Toilets.svelte
@@ -2,6 +2,7 @@
   import {goToLocation} from '../../dataStores/locationStore.js'
   import {onMount} from 'svelte'
   import toiletsImageSrc from '../../../assets/teodororjansen/toilets.png'
+  import door1Handle from '../../../assets/teodororjansen/doorHandle.png'
   const door1Url = 'https://vika.vgs.no/'
   export let location
   let mainElement
@@ -17,7 +18,9 @@
   <button on:click={() => goToLocation('dh')}>Exit to hallway</button>
 </div>
 
-<a href="{door1Url}" target="_blank"></a>
+<a href="{door1Url}" target="_blank">
+  <img src="{door1Handle}" alt="doorHandle" width="" height="" style="position: absolute; top: 50%; left: 50%;">
+</a>
 
 <style>
   .toilets{
