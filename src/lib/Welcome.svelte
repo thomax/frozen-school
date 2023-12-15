a0bbcec45e45d656105e4f011e27b0dff749671c
Philip Ingulstad Torrissen <phtoa001@osloskolen.no>
2023-12-15 14:35:27 +0100

    fix changes

--- src/lib/Welcome.svelte
+++ src/lib/Welcome.svelte
@@ -1,13 +1,100 @@
 <script>
-  import {setGameStatus} from './dataStores/stateStore.js'
+  import { setGameStatus } from './dataStores/stateStore.js'
 
-  // Handle update of game state
   function handleStatusChange(newStatus) {
     setGameStatus(newStatus)
   }
 </script>
 
+<<<<<<< Updated upstream
 <div id="welcome-component">
   <h1>Welcome!</h1>
   <button on:click={() => handleStatusChange('gameRunning')}>Play the game!</button>
+=======
+<style>
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  @keyframes bounce {
+    0%, 20%, 50%, 80%, 100% {
+      transform: translateY(0);
+    }
+    40% {
+      transform: translateY(-20px);
+    }
+    60% {
+      transform: translateY(-10px);
+    }
+  }
+
+  body, html {
+    height: 100%;
+    margin: 0;
+  }
+
+  .gameStateBox {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    animation: fadeIn 1s ease-out;
+  }
+
+  h1, p, button {
+    font-family: 'Silkscreen', monospace; /* Use the Silkscreen font */
+  }
+
+  h1 {
+    font-size: 3em; /* Increased header size */
+    color: #2254ca;
+  }
+
+  p {
+    font-size: 1.2em;
+    color: #000000;
+    text-align: center;
+    margin: 20px;
+    background-color: #f8f8f8; 
+    padding: 15px; 
+    border-radius: 10px; 
+  }
+
+  button {
+    font-size: 1.5em;
+    padding: 15px 30px;
+    background: linear-gradient(45deg, #2d39c0, #55a5d7);
+    color: white;
+    border: none;
+    border-radius: 2px;
+    cursor: pointer;
+    animation: bounce 1s infinite;
+    transition: transform 0.3s;
+    font-smooth: never;
+  }
+
+  button:hover {
+    transform: scale(1.1);
+  }
+</style>
+
+<!-- Link til Google fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Silkscreen&display=swap" rel="stylesheet">
+
+<div id="welcome-component" class="gameStateBox">
+  <h1>Welcome to the Frozen School!</h1>
+  <p>
+    You find yourself locked in a freezing school. Your mission is to explore the rooms,
+    complete tasks, and survive the cold. Can you survive the cold?
+  </p>
+  <button on:click={() => handleStatusChange('gameRunning')}>Start the game!</button>
+>>>>>>> Stashed changes
 </div>
--- src/lib/locations/philiptorrissen/StrangeOffice.svelte
+++ src/lib/locations/philiptorrissen/StrangeOffice.svelte
@@ -10,17 +10,48 @@
   import costume8 from '../../../assets/philiptorrissen/costume8.png'
   import costume9 from '../../../assets/philiptorrissen/costume9.png'
   import costume10 from '../../../assets/philiptorrissen/costume10.png'
+<<<<<<< Updated upstream
   import background from '../../../assets/philiptorrissen/Background.jpg'
   import {onMount} from 'svelte'
+=======
+  import background from '../../../assets/philiptorrissen/background.jpg'
+  import { character, changeTemperature, changeHealth } from '../../dataStores/characterStore.js'
+  import { onMount } from 'svelte'
+  import GameOver from './gameOver.svelte';
+>>>>>>> Stashed changes
 
   export let location
   let maxAttempts = 10
   let guess = ''
-  let wordList = ['skole', 'kaldt', 'varmt']
+  let wordList = [
+    'ices',
+    'snow',
+    // ... (remaining words)
+  ];
   let attemptsLeft = maxAttempts
   let word = getRandomWord()
   let displayWord = Array.from(word).fill('_')
   let guessedLetters = []
+  let incorrectGuesses = []
+
+  function handleChangeTemperature(amount) {
+    changeTemperature(amount)
+  }
+
+  function showGameOverScreen() {
+    // Render the game over screen when the game ends
+    import('./gameOver.svelte').then(({ default: GameOver }) => {
+      const gameContainer = document.querySelector('.game-container');
+      gameContainer.innerHTML = '';
+      new GameOver({
+        target: gameContainer,
+      });
+    });
+  }
+
+  function handleChangeHealth(amount) {
+    changeHealth(amount)
+  }
 
   function getRandomWord() {
     return wordList[Math.floor(Math.random() * wordList.length)]
@@ -30,6 +61,7 @@
     alert(word)
   }
 
+<<<<<<< Updated upstream
   function makeGuess() {}
 
   function resetGame() {}
@@ -41,10 +73,70 @@
       mainElement.style.backgroundSize = 'cover'
       mainElement.style.backgroundPosition = 'top 100px'
       mainElement.style.backgroundSize = 'contain'
+=======
+  function makeGuess() {
+    if (guess.length !== 1 || !/[a-zA-Z]/.test(guess)) {
+      alert("Please enter a valid single letter guess.")
+      return
+    }
+
+    if (guessedLetters.includes(guess)) {
+      alert("You already guessed this letter.")
+      return
+    }
+
+    guessedLetters.push(guess)
+
+    if (word.includes(guess)) {
+      word.split('').forEach((letter, index) => {
+        if (letter === guess) {
+          displayWord[index] = guess
+        }
+      })
+
+      if (!displayWord.includes('_')) {
+        handleChangeTemperature(40)
+        alert("Congratulations! You guessed the word.")
+        // Reset the guess variable to an empty string
+        guess = ''
+        return
+      }
+    } else {
+      attemptsLeft--
+      incorrectGuesses.push(guess)
+      console.log('Incorrect guesses:', incorrectGuesses);
+      incorrectGuesses = [...incorrectGuesses];
+
+      if (attemptsLeft === 0) {
+        alert(`Sorry, you ran out of attempts. The correct word was "${word}".`)
+        showGameOverScreen();
+        handleChangeHealth(-50)
+        // Reset the guess variable to an empty string
+        guess = ''
+        return
+      }
+    }
+    // Reset the guess variable to an empty string
+    guess = ''
+  }
+
+  function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+      makeGuess();
+    }
+  }
+
+  onMount(() => {
+    const mainElement = document.querySelector('.game-container')
+    if (mainElement) {
+      mainElement.style.background = `url('${background}') no-repeat center center`
+      mainElement.style.backgroundSize = 'cover';
+>>>>>>> Stashed changes
     }
   })
 </script>
 
+<<<<<<< Updated upstream
 <div class="game-container">
   <div class="background-image">
     <h1>{location.title}</h1>
@@ -87,5 +179,117 @@
     left: 50%;
     transform: translateX(30%);
     transform: translateY(30%);
+=======
+<style>
+  body {
+    margin: 0;
+    overflow: hidden;
+    font-family: 'Arial', sans-serif;
+  }
+
+  .game-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    background-color: transparent;
+    color: white;
+  }
+
+  .hangman {
+    max-width: 100px;
+    height: auto;
+    margin: 10px 0;
+  }
+
+  h1 {
+    font-size: 2em;
+    margin-bottom: 20px;
+  }
+
+  p {
+    margin: 10px 0;
+    background-color: rgba(255, 255, 255, 1);
+    color: black;
+    padding: 5px;
+    border-radius: 5px;
+  }
+
+  input {
+    margin-top: 10px;
+    padding: 5px;
+    border: 1px solid white;
+    border-radius: 5px;
+  }
+
+  .button-container {
+    display: flex;
+    margin-top: 10px;
+  }
+
+  button {
+    cursor: pointer;
+    padding: 5px 10px;
+    margin-right: 10px;
+    background-color: rgba(255, 255, 255, 0.9); /* Adjusted opacity */
+    border: 1px solid white;
+    border-radius: 5px;
+    color: black; /* Change text color for better contrast */
+    transition: background-color 0.3s ease;
+  }
+
+  button:hover {
+    background-color: rgba(255, 255, 255, 1); /* Fully opaque white background on hover */
+  }
+
+  button.invisible {
+    background-color: rgba(255, 255, 255, 0.1); /* Fully opaque white background on hover */
+    color: rgba(255, 255, 255, 0.1);
+    border: 0px solid white;
+  }
+
+  .incorrect-guesses {
+    margin-top: 10px;
+>>>>>>> Stashed changes
   }
 </style>
+
+<div class="game-container">
+  <h1>{location.title}</h1>
+  {#if attemptsLeft === 10}
+    <img class="hangman" src={costume1} alt="Image for Attempt 10">
+  {:else if attemptsLeft === 9}
+    <img class="hangman" src={costume2} alt="Image for Attempt 9">
+  {:else if attemptsLeft === 8}
+    <img class="hangman" src={costume3} alt="Image for Attempt 8">
+  {:else if attemptsLeft === 7}
+    <img class="hangman" src={costume4} alt="Image for Attempt 7">
+  {:else if attemptsLeft === 6}
+    <img class="hangman" src={costume5} alt="Image for Attempt 6">
+  {:else if attemptsLeft === 5}
+    <img class="hangman" src={costume6} alt="Image for Attempt 5">
+  {:else if attemptsLeft === 4}
+    <img class="hangman" src={costume7} alt="Image for Attempt 4">
+  {:else if attemptsLeft === 3}
+    <img class="hangman" src={costume8} alt="Image for Attempt 3">
+  {:else if attemptsLeft === 2}
+    <img class="hangman" src={costume9} alt="Image for Attempt 2">
+  {:else if attemptsLeft === 1}
+    <img class="hangman" src={costume10} alt="Image for Attempt 1">
+  {/if}
+
+  <p>Attempts left: {attemptsLeft}</p>
+  <p>{displayWord.join(' ')}</p>
+  <div class="incorrect-guesses">
+    <p>Incorrect guesses: {incorrectGuesses.join(', ')}</p>
+  </div>
+  <div>
+    <input type="text" bind:value={guess} placeholder="Guess the letter" on:keydown={handleKeyPress}>
+  </div>
+  <div class="button-container">
+    <button on:click={() => makeGuess()}>Make Guess</button>
+    <button class="invisible" on:click={() => hint()}>?</button>
+    <button on:click={() => goToLocation('hall')}>Exit to hallway</button>
+  </div>
+</div>
