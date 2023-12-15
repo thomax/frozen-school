fc3a1985e1940e5fa46e8bb6afba67e8da805371
Jenny Schou Haugestøl <jehaa008@osloskolen.no>
2023-12-15 15:31:11 +0100

    get rid of unsused stuff

--- src/lib/locations/jscode1003/Fire.svelte
+++ src/lib/locations/jscode1003/Fire.svelte
@@ -4,12 +4,7 @@
     export let location
     import LockerWithFire from '../../../assets/jscode1003/WithFire.png'
     let mainElement
-<<<<<<< Updated upstream
     import Nisse from './Nisse.svelte';
-=======
-    import {changeTemperature} from '../../dataStores/characterStore.js'
-    import Nisse from '../../../assets/jscode1003/nisse.png'
->>>>>>> Stashed changes
     let message = "30+ in temprature! Paper was set on fire and removed from your inventory."
   
   
@@ -18,22 +13,12 @@
       mainElement.style.backgroundSize = 'cover'
     })
 
-<<<<<<< Updated upstream
-=======
-
-    changeTemperature(30);
->>>>>>> Stashed changes
   </script>
 
   <div class="img" bind:this={mainElement}>
     <h1>{location.title}</h1>
     <button on:click={() => goToLocation('dh')}>Exit to hallway</button>
-<<<<<<< Updated upstream
     <Nisse message= {message}></Nisse>
-=======
-    <p class= "message">{message}</p>
-    <img class="nisse" src="{Nisse}" alt="nisse">
->>>>>>> Stashed changes
   </div>
   
   <style>
@@ -43,42 +28,7 @@
       position: relative;
       margin: auto;
     }
-  
-    .button {
-      width: 150px;
-      position: absolute; 
-      top: 65%; 
-      left: 45%;
-    }
 
-    .nisse {
-    width: 90px;
-    position: absolute; 
-    top: 60%; 
-    left: 0.001%;
-    }
-
-    .message {
-    color: black;
-<<<<<<< Updated upstream
-    background-color: #ff7d7df0;
-    border: 3px solid black;
-    padding: 15px;
-    width: 150px;
-    height: auto;
-=======
-    background-color: #ff7d7d;
-    border: 3px solid black;
-    padding: 15px;
-    width: 150px;
-    height: 90px;
->>>>>>> Stashed changes
-    font-family: Arial, Helvetica, sans-serif;
-    text-align: center;
-    position: absolute; 
-    top: 55%; 
-    left: 9%;
-  }
   </style>
   
   
--- src/lib/locations/jscode1003/LockerWithFireplace.svelte
+++ src/lib/locations/jscode1003/LockerWithFireplace.svelte
@@ -4,18 +4,12 @@
   export let location
   import DefaultLocker from '../../../assets/jscode1003/LockerWithoutFire.png'
   import Button from '../../../assets/jscode1003/button.jpg'
-  import Nisse from '../../../assets/jscode1003/nisse.png'
   let mainElement
   import {removeFromInventory} from '../../dataStores/characterStore.js'
   import {changeTemperature} from '../../dataStores/characterStore.js'
   let message = ""
-<<<<<<< Updated upstream
   import Nisse from './Nisse.svelte'
 
-=======
-  import { addToInventory } from '../../dataStores/characterStore.js';
->>>>>>> Stashed changes
-  
   onMount(() => {
     mainElement.style.background = `url('${DefaultLocker}')  no-repeat center center`
     mainElement.style.backgroundSize = 'cover'
@@ -26,22 +20,11 @@
     if (paperOK) {
       goToLocation('fire')
       message = "Paper was set on fire"
-<<<<<<< Updated upstream
       changeTemperature(30);
       return
     }
     
     message = "You need paper to light the fireplace. Clue: Library"
-=======
-      return
-    }
-    message = "You need paper to light the fireplace"
-  }
-
-  function handlePaper(){
-    addToInventory('Paper')
-    message='Paper is added to inventory'
->>>>>>> Stashed changes
   }
 
 
@@ -51,19 +34,9 @@
 
 <div class="img" bind:this={mainElement}>
   <h1>{location.title}</h1>
-<<<<<<< Updated upstream
   <button on:click={() => goToLocation('hall')}>Exit to hallway</button>
   <img class=button src="{Button}" alt="fireplace" on:click={handleClickFire}>
   <Nisse message= {message}></Nisse>
-=======
-  <div>Locker with fireplace</div>
-
-  <button on:click={() => goToLocation('dh')}>Exit to hallway</button>
-  <button on:click={handlePaper}>Add paper to inventory</button>
-  <img class=button src="{Button}" alt="fireplace" on:click={handleClickFire}>
-  <p class= "message">{message}</p>
-  <img class="nisse" src="{Nisse}" alt="nisse">
->>>>>>> Stashed changes
 </div>
 
 
@@ -81,39 +54,6 @@
     position: absolute; 
     top: 65%; 
     left: 45%;
-  }
-
-  .nisse {
-    width: 90px;
-    position: absolute; 
-    top: 60%; 
-    left: 0.001%;
-<<<<<<< Updated upstream
-    }
-
-    .message {
-    color: black;
-    background-color: #7dffffef;
-    border: 3px solid black;
-    padding: 15px;
-    width: 150px;
-    height: auto;
-=======
-  }
-  
-  .message {
-    color: black;
-    background-color: #7dcdff;
-    border: 3px solid black;
-    padding: 15px;
-    width: 125px;
-    height: 60px;
->>>>>>> Stashed changes
-    font-family: Arial, Helvetica, sans-serif;
-    text-align: center;
-    position: absolute; 
-    top: 55%; 
-    left: 9%;
   }
 </style>
 
